8c12b32188e5318ceb00ebeb6723b40fbc036ee2
docavd <docavd@yandex.ru>
2019-12-23 18:12:35 +0300

    custom table view cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by 111 on 12/23/19.
+//  Copyright © 2019 111. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+}
